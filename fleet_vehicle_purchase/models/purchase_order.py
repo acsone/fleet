6f6dc5b875419b93ffa5669ba177692c9101b7b7
Lindsay <lindsay.marion@acsone.eu>
2024-10-15 15:26:47 +0200

    [IMP] Add the possibility to have more than 1 vehicle by PO
    Initially, there was only one vehicle for one PO.
    Now, if the vehicle is set on the PO, all the PO lines are related to this vehicle
    But if the vehicle is not set on the PO, the user can decide to have different vehicles on every line.
    Use case is a grouped purchase of several items for several vehicles.

--- fleet_vehicle_purchase/models/purchase_order.py
+++ fleet_vehicle_purchase/models/purchase_order.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Dixmit
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -14,8 +14,30 @@ class PurchaseOrder(models.Model):
 class PurchaseOrderLine(models.Model):
 
     _inherit = "purchase.order.line"
+    fleet_vehicle_id = fields.Many2one(
+        comodel_name="fleet.vehicle",
+        compute="_compute_fleet_vehicle_id",
+        store=True,
+        readonly=False,
+    )
+    fleet_vehicle_from_po = fields.Boolean(
+        compute="_compute_fleet_vehicle_from_po", store=True
+    )
+
+    @api.depends("fleet_vehicle_from_po")
+    def _compute_fleet_vehicle_id(self):
+        for rec in self:
+            if rec.fleet_vehicle_from_po:
+                rec.fleet_vehicle_id = rec.order_id.fleet_vehicle_id
+
+    @api.depends("order_id", "order_id.fleet_vehicle_id")
+    def _compute_fleet_vehicle_from_po(self):
+        for rec in self:
+            rec.fleet_vehicle_from_po = bool(rec.order_id.fleet_vehicle_id)
 
     def _prepare_account_move_line(self, move=False):
+        self.ensure_one()
         result = super()._prepare_account_move_line(move)
-        result["vehicle_id"] = self.order_id.fleet_vehicle_id.id
+        if self.fleet_vehicle_id:
+            result["vehicle_id"] = self.fleet_vehicle_id.id
         return result
